d68b5ad74fc781f8ac4292d06b390ec7a484a4a0
keulia <55962564+keulia@users.noreply.github.com>
2021-11-08 00:27:15 +0100

    Add files via upload

--- Unitat 5 - Python 3 básic/Python 3 - Activitats avaluables 02/Entrevista 01/Ejercicio1.py
+++ Unitat 5 - Python 3 básic/Python 3 - Activitats avaluables 02/Entrevista 01/Ejercicio1.py
@@ -1,16 +1,23 @@
-from arepl_dump import dump
 from functools import reduce
 
-#Ejemplo map
+
+# lamba function
+# We are taking the provided value and converting it into upper caps and printing the result
+upper = lambda a: str.upper(a)
+print(upper("cass"))
+
+
+values = input("Enter numbers: ").split(" ")
+
+# map(), list() and int() example
+values = list(map(lambda x: int(x), values))
 
 #Ejemplo filter
 #Utilitzant “filter()”, elimina de la cadena anterior els números menors que 10.
-valores = [1, 2, 3, 4, 5, 6, 7, 8, 9,11,25]
-print(valores)
-impares = list(filter(lambda x : x >= 10, valores))
-valores= impares
+print(values)
+values = list(filter(lambda x : x >= 10, values))
 
 #Ejemplo reduce
 #Estamos realizando un cálculo acumulativo
-suma = reduce(lambda x, y: x * y, valores)
+suma = reduce(lambda x, y: x * y, values)
 print(suma)

64410353ebb84584978b421ea193787e8bdc8e6e
keulia <55962564+keulia@users.noreply.github.com>
2021-11-18 17:02:13 +0100

    Add files via upload

---
+++ Unitat 5 - Python 3 básic/Python 3 - Activitats avaluables 02/Entrevista 5/ClassTeacher.py
@@ -0,0 +1,9 @@
+
+class Teacher:
+    def __init__(self, name, subject):
+        self.name = name
+        self.subject = subject
+
+    def returnTeacher():
+        return self.name
+        return self.subject

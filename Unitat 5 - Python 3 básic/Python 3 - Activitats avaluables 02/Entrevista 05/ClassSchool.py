0d77db819206957ab6959603ff00a21368f6a82f
keulia <55962564+keulia@users.noreply.github.com>
2021-11-18 17:03:00 +0100

    Add files via upload

---
+++ Unitat 5 - Python 3 básic/Python 3 - Activitats avaluables 02/Entrevista 05/ClassSchool.py
@@ -0,0 +1,11 @@
+
+class School:
+
+    def __init__(self, name, location, responsible):
+        self.name = name
+        self.location = location
+        self.responsible = responsible
+
+
+def __main():
+    School("Serra perenxisa","torrent","srta. algo")

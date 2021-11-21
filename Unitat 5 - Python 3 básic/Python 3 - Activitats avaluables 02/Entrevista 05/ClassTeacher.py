e5c2027a2ee42ace61fac59fb73a0202edab77d3
keulia <55962564+keulia@users.noreply.github.com>
2021-11-21 21:46:14 +0100

    Delete ClassTeacher.py

--- Unitat 5 - Python 3 básic/Python 3 - Activitats avaluables 02/Entrevista 05/ClassTeacher.py
+++
@@ -1,9 +0,0 @@
-
-class Teacher:
-    def __init__(self, name, subject):
-        self.name = name
-        self.subject = subject
-
-    def returnTeacher():
-        return self.name
-        return self.subject

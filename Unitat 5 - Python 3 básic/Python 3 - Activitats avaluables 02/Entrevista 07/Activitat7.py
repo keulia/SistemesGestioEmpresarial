7b829819b055bf1ae049de721cc365bcfcc4efa3
keulia <55962564+keulia@users.noreply.github.com>
2021-11-21 21:23:26 +0100

    Add files via upload

--- Unitat 5 - Python 3 básic/Python 3 - Activitats avaluables 02/Entrevista 07/Activitat7.py
+++ Unitat 5 - Python 3 básic/Python 3 - Activitats avaluables 02/Entrevista 07/Activitat7.py
@@ -1,69 +1,98 @@
 """
-1 - Recieve the name of 2 different documentos
-2 - Make 2 functions -"isPalindrome" "isPrime"
+*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧
+|￣￣￣￣￣￣￣￣￣￣￣|
+    Exercise 7
+|＿＿＿＿＿＿＿＿＿＿＿|
+⠀⠀⠀⠀⠀⠀(\__/) ||
+⠀⠀⠀⠀⠀⠀(•ㅅ•) ||
+⠀⠀⠀⠀⠀⠀/ 　 づ
 
-3 - Prime function - that number will get divided until half of that number. so if: 10 it will get divided until 5
-  - if number divided by 2 = 0 then number isn't prime
-  - if number isn't divisable by anything expect 0 and 1, number is prime
+Author: Cassandra Sowa Candela ♡ ´･ᴗ･ `♡
+2ºDAM
+November 2021
 
- 4 - Palindrome function - turn the number of the line in the opposite order, starting from the right
-   - compare original number with new number
-   - if they equal the same then isPalindrome = true
+⠀                                                 ⠀∩∩
+                                                ♡(｡･x･)♡  
 
+   ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄                                                                     ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄ 
+   █▒▒░░░░░░░░░▒▒█                                                                     █▒▒░░░░░░░░░▒▒█
+    █░░█░░░░░█░░█                                                                       █░░█░░░░░█░░█
+ ▄▄  █░░░▀█▀░░░█  ▄▄                                                                 ▄▄  █░░░▀█▀░░░█  ▄▄
+█░░█ ▀▄░░░░░░░▄▀ █░░█                                                               █░░█ ▀▄░░░░░░░▄▀ █░░█
+
+*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡
 """
 
+from numpy import sqrt  #import this for calculating mathematical operations.
 
-import sys
-import doctest
+file = open("Users\HP\Documents\input.txt")  #open the input file.
+lines = file.read().splitlines() #line is the list that store the input file elements.
+file.close()
 
-"""
-print("Introduce the file we will read from: ")
-textFile = sys.argv
+palindrome_list = [] #list that stores palindromic strings.
+prime_list = []      #list that stores prime numbers.
 
-"""
-def isPrime(lines):
-    if not int(lines)%2==0: # If the number in that line isn't divisible by 2 do
-        n = lines%2 # im adding that number divided by 2 so i can divide the number to a max of that answer
-        d = 3 # So i start diving by 3 each line
-        for x in int(n): # So it goes from X to half of that number
-            if not lines%d==0: # starts by number divided by 3, if it's divisible return "false" if it is not divisible by any number return true
-                if d == (n-1): # If d is the same number 
-                    return True
-                else:
-                    d=d+1 # number that is used to divide increases by one for the next loop
-            else:
-                return False
-    else:
+#for palindrome_list 
+for i in range(len(lines)):
+    y=len(lines[i])
+    flag = 0
+    for j in range(y):
+        if(lines[i][j] != lines[i][y-j-1]):
+            flag = 1
+    if(flag == 0):
+        palindrome_list.append(lines[i])
+            
+#for prime_list
+def isPrime(num):
+    if num<=1:
         return False
+    if num==2:
+        return True
+    if num%2==0:
+        return False
+    for i in range(3,int(sqrt(num))+1):
+        if num%i==0:
+            return False
+    return True
 
-def isPalindrome(lines):
-    # Copy in reverse
-    reverse = lines[::-1] # We reverse the order
-    if lines == int(reverse): # If the reversed version is the same as the original then it will return true
-        return true
+for i in lines:
+    i=int(i)
+    if(isPrime(i)):
+        prime_list.append(i)
 
-countPalindrome = 0 # To count how many Palindrome numbers we have
-countPrime = 0 # To count how many Prime numbers we have
-# With r we are opening the document in a reading mode
-# Using with will close automatically our text document once we are done with it
+#getting length of the common_list which can be checked which has less elements
+q = 0
+if(len(palindrome_list)<len(prime_list)):
+    q=len(palindrome_list)
+else:
+    q=len(prime_list)
 
-bothNUmbers = []
-with open('readme.txt', 'r') as f:
-    lines = f.readlines()
-    if isPalindrome(f):
-        countPalindrome+1
-        if isPrime(f) == true:
-            bothNUmbers.append(f)
+#common_list    
+common_list = [] #common_list which contains both prime and palindrome-one
+#checking equal items
+for i in range(q):
+    if(prime_list[i] == int(palindrome_list[i])):
+        common_list.append(prime_list[i])
 
-    if isPrime(f) == true:
-        countPrime+1
+#to append common_list in file
+with open('output.txt', 'a') as f:
+    f.write(f"There are {len(palindrome_list)} palindromic numbers.\n")
+    f.write(f"There are {len(prime_list)} prime numbers.\n")
+    for i in common_list:
+        f.write(str(i))
+        f.write("\n")
+f.close()
+#open file and read contents of output.txt file
+f = open("output.txt", "r")
+print(f.read())
+"""
+*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡
+⠀⠀⠀⠀°. ♡
+⠀⠀⠀⠀. °
+⠀⠀＿♡
+⊂⊂ ・）
+⠀/　 |
+⊂＿__u
 
-# With w we will be writting in the text
-with open('readme.txt', 'w') as f:
-    f.write('There are ',countPalindrome,' Palindrome numbers')
-    f.write('/n')
-    f.write('There are ',countPrime,' Prime numbers')
-    f.write('/n')
-
-
-
+*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧
+"""

4c4a47ad9906958a9633b09dc0aa6934cf40a506
keulia <55962564+keulia@users.noreply.github.com>
2021-11-21 17:21:55 +0100

    Add files via upload

--- Unitat 5 - Python 3 básic/Python 3 - Activitats avaluables 02/Entrevista 12/Activitat12.py
+++ Unitat 5 - Python 3 básic/Python 3 - Activitats avaluables 02/Entrevista 12/Activitat12.py
@@ -1,15 +1,53 @@
-# adds image processing capabilities
-from PIL import Image
-# will convert the image to text string
+"""
+*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧
+|￣￣￣￣￣￣￣￣￣￣￣|
+    Exercise 3
+|＿＿＿＿＿＿＿＿＿＿＿|
+⠀⠀⠀⠀⠀⠀(\__/) ||
+⠀⠀⠀⠀⠀⠀(•ㅅ•) ||
+⠀⠀⠀⠀⠀⠀/ 　 づ
+
+Author: Cassandra Sowa Candela ♡ ´･ᴗ･ `♡
+2ºDAM
+November 2021
+
+⠀                                                 ⠀∩∩
+                                                ♡(｡･x･)♡  
+
+   ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄                                                                     ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄ 
+   █▒▒░░░░░░░░░▒▒█                                                                     █▒▒░░░░░░░░░▒▒█
+    █░░█░░░░░█░░█                                                                       █░░█░░░░░█░░█
+ ▄▄  █░░░▀█▀░░░█  ▄▄                                                                 ▄▄  █░░░▀█▀░░░█  ▄▄
+█░░█ ▀▄░░░░░░░▄▀ █░░█                                                               █░░█ ▀▄░░░░░░░▄▀ █░░█
+
+*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡
+"""
+
+from PIL import Image, ImageEnhance
 import pytesseract
-
-# assigning an image from the source path
-# ADD PIC
-img = Image.open('image_test/virginia-quote.jpg')
-# converts the image to result and saves it into result variable
-result = pytesseract.image_to_string(img)
-
-with open('text_result.txt', mode ='w') as file:
+# We are assaigning an image
+img = Image.open('test.png')
+# Here we are editting the image, we are adding sharpness and contrast
+enhancer1 = ImageEnhance.Sharpness(img)
+enhancer2 = ImageEnhance.Contrast(img)
+img_edit = enhancer1.enhance(20.0)
+img_edit = enhancer2.enhance(1.5)
+# We are saving the editted image
+img_edit.save("edited_image.png")
+# Converts the text of the image into a string
+result = pytesseract.image_to_string(img_edit)
+with open('text_result2.txt', mode ='w') as file:
  file.write(result)
  print("ready!")
+ 
+ """
+*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡
+⠀⠀⠀⠀°. ♡
+⠀⠀⠀⠀. °
+⠀⠀＿♡
+⊂⊂ ・）
+⠀/　 |
+⊂＿__u
 
+*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧
+"""

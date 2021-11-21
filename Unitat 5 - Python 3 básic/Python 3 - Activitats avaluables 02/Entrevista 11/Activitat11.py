48ecc05c0af4e1c4e3ec7d22fddf9731374d1314
keulia <55962564+keulia@users.noreply.github.com>
2021-11-21 23:47:12 +0100

    Add files via upload

---
+++ Unitat 5 - Python 3 básic/Python 3 - Activitats avaluables 02/Entrevista 11/Activitat11.py
@@ -0,0 +1,93 @@
+"""
+*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧
+|￣￣￣￣￣￣￣￣￣￣￣|
+    Exercise 11
+|＿＿＿＿＿＿＿＿＿＿＿|
+⠀⠀⠀⠀⠀⠀(\__/) ||
+⠀⠀⠀⠀⠀⠀(•ㅅ•) ||
+⠀⠀⠀⠀⠀⠀/ 　 づ
+
+Author: Cassandra Sowa Candela ♡ ´･ᴗ･ `♡
+2ºDAM
+November 2021
+
+⠀                                                 ⠀∩∩
+                                                ♡(｡･x･)♡  
+
+   ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄                                                                     ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄ 
+   █▒▒░░░░░░░░░▒▒█                                                                     █▒▒░░░░░░░░░▒▒█
+    █░░█░░░░░█░░█                                                                       █░░█░░░░░█░░█
+ ▄▄  █░░░▀█▀░░░█  ▄▄                                                                 ▄▄  █░░░▀█▀░░░█  ▄▄
+█░░█ ▀▄░░░░░░░▄▀ █░░█                                                               █░░█ ▀▄░░░░░░░▄▀ █░░█
+
+*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡
+"""
+from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+import os
+import csv
+import hashlib
+
+class Login(Screen):
+
+    
+    def do_login(self, loginText, passwordText):
+        app = App.get_running_app()
+
+        # Transforming the password into sha1
+        sha1 = hashlib.sha1()
+        key =passwordText.encode('utf-8')
+        sha1.update(key)
+
+        # Opens the file where we have stored the usernames and their passwords
+        with open("./users.csv", newline='') as File:  
+            readera = csv.reader(File) # Reads the file
+            LogginIn = False
+            # Checking that the username and the password match
+            for row in readera:
+                if(row[0] == loginText and row[1] == sha1.hexdigest().upper()):
+                    LogginIn = True
+            if(LogginIn):
+                self.ids['works'].text = "OK" # If it works it will print "OK"
+            else:
+                self.ids['works'].text = "ERROR" # If the username or password doesn't exist it will print "ERROR"
+
+# Class LoginApp with a username and a password
+class LoginApp(App):
+    username = StringProperty(None)
+    password = StringProperty(None)
+
+    # We use this to start the app
+    def build(self):
+        manager = ScreenManager()
+        manager.add_widget(Login(name='login'))
+        return manager
+
+    def get_application_config(self):
+        if(not self.username):
+            return super(LoginApp, self).get_application_config()
+
+        conf_directory = self.user_data_dir + '/' + self.username
+
+        if(not os.path.exists(conf_directory)):
+            os.makedirs(conf_directory)
+
+        return super(LoginApp, self).get_application_config(
+            '%s/config.cfg' % (conf_directory)
+        )
+
+# Starts the app
+if __name__ == '__main__':
+    LoginApp().run()  
+"""
+*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡*✲ﾟ*｡⋆♡
+⠀⠀⠀⠀°. ♡
+⠀⠀⠀⠀. °
+⠀⠀＿♡
+⊂⊂ ・）
+⠀/　 |
+⊂＿__u
+
+*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧ʕᵔᴥᵔ♥ʔ*˖⁺*˖⁺♡˖°♥°˖✧
+"""
